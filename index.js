9bb2b303fbe41df91f9cf9bb47ac6bd001f7ad73
Paweł Kumorowski <kumorowski.p@gmail.com>
2022-03-08 13:41:51 +0000

    dialer

--- index.js
+++ index.js
@@ -1,71 +1,23 @@
 const express = require('express');
 const app = express();
+const Dialer = require('dialer').Dialer;
+
+const config = {
+   url: 'https://uni-call.fcc-online.pl',
+   login: '<login>',
+   password: '<haslo>'
+};
+
+Dialer.configure(config);  
 
 app.listen(3000, () => {
-   console.log('app listening');
+   console.log('app listening on port 3000');
 });
 
-
-app.get('/', (req, res) => {
+app.get('/call/:number1/:number2', (req, res) => {
+   const number1 = req.params.number1;
+   const number2 = req.params.number2;
+  
+   Dialer.call(number1,number2);
    res.json({success: true});
 })
-
-app.get('/call/:status', (req, res) => {
-   let id = null;
-   const status = req.params.status;
-   getRandomId((randomId) => {
-       console.log(randomId);
-       id = randomId;
-   });
-   console.log('sending response');
-   res.json({'success': true, id: id, status: status})
-})
-
-function getRandomId(callback) {
-   setTimeout(() => {
-       var random = Math.round(Math.random()*10000)
-       callback(random);
-   }, Math.round(Math.random()*1000))
-}
-
-function getPromise() {
-   return new Promise((resolve, reject) => {
-       const a = Math.random();
-       resolve(a);
-   })
-}
-
-
-app.get('/promise', (req, res) => {
-   getPromise().then((result) => {
-       res.json({success: true, result: result});
-   })
-})
-
-
-app.get('/promise2', (req, res) => {
-   let results = [];
-   getPromise()
-   .then((result) => {
-       results.push(result)
-   })
-   .then(getPromise)
-   .then((result) => {
-       results.push(result)
-   }).then(() => {
-       res.json({success:true, results: results})
-   })
-})
-
-
-app.get('/promise3', async (req, res) => {
-   let results = [];
-   let random1 = await getPromise();
-   results.push(random1);
-   let random2 = await getPromise();
-   results.push(random2);
-   res.json({success:true, results: results})
-})
-
-
-
