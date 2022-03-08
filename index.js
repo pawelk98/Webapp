7a0c55b5d032c86ad549436c4414796ec8e0b5b2
Paweł Kumorowski <kumorowski.p@gmail.com>
2022-03-08 14:43:24 +0000

    post calling

--- index.js
+++ index.js
@@ -1,23 +1,43 @@
 const express = require('express');
 const app = express();
 const Dialer = require('dialer').Dialer;
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const config = {
    url: 'https://uni-call.fcc-online.pl',
-   login: '<login>',
-   password: '<haslo>'
+   login: 'login',
+   password: 'haslo'
 };
 
 Dialer.configure(config);  
+app.use(cors());
+app.use(bodyParser.json());
 
 app.listen(3000, () => {
    console.log('app listening on port 3000');
 });
 
-app.get('/call/:number1/:number2', (req, res) => {
+let bridge = null;
+
+app.get('/call/:number1/:number2', async (req, res) => {
    const number1 = req.params.number1;
    const number2 = req.params.number2;
   
-   Dialer.call(number1,number2);
+   bridge = await Dialer.call(number1, number2);
    res.json({success: true});
-})
+});
+
+app.get('/status', async (req, res) => {
+   let status = 'NONE';
+   if(bridge != null) {
+      status = await bridge.getStatus();
+   }
+   res.json({success: true, status: status});
+});
+
+app.post('/call', async (req, res) => {
+   const body = req.body;
+   bridge = await Dialer.call(body.number1, body.number2);
+   res.json({ success: true })
+});
