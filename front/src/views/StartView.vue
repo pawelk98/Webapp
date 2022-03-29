3dd5ea44dfe2d95ec79f27b6132048cc4f0abb3b
Paweł Kumorowski <kumorowski.p@gmail.com>
2022-03-29 13:47:51 +0000

    fix

--- front/src/views/StartView.vue
+++ front/src/views/StartView.vue
@@ -19,7 +19,7 @@ export default defineComponent({
   }),
   methods: {
     async call() {
-      const responseStream = await fetch('${config.api.url}${config.api.prefix}/call', {
+      const responseStream = await fetch(`${config.api.url}${config.api.prefix}/call`, {
         method: 'POST',
         headers: { 'Content-type': 'application/json; charset=UTF-8' },
         body: JSON.stringify({ number: this.number }),
